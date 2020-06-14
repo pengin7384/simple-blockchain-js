d63e9e35141838b42787aca0912a9839bfcf0a93
csh <csh@comcom.ai>
2020-06-14 15:31:56 +0900

    Update test code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('test');
+const Block = require('./block');
+const Blockchain = require('./blockchain');
+const time = require('./time');
+const simpleCoin = new Blockchain('Simple');
+
+function main() {
+  simpleCoin.addBlock(new Block(time.getCurrentTime(), { amount: 10 }));
+  simpleCoin.addBlock(new Block(time.getCurrentTime(), { amount: 20 }));
+  simpleCoin.addBlock(new Block(time.getCurrentTime(), { amount: 30 }));
+
+  console.log(simpleCoin.isChainValid());
+  simpleCoin.chain[1].data = { amount: 50 };
+  console.log(simpleCoin.isChainValid());
+}
+
+
+main();
